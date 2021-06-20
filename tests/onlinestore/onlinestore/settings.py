ef4148e2c315ff2ac42d8e2d74cede33cf01dfd4
Kenji Emura <kenjiemura@gmail.com>
2021-06-20 23:46:50 +0900

    Dummy django app that displays products

--- tests/onlinestore/onlinestore/settings.py
+++ tests/onlinestore/onlinestore/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'products'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = "uploads"
+MEDIA_URL = "/media/"
