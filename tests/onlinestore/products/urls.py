0edc63ccbafeeed879f1571ee8eadab361f4e9c9
Kenji Emura <kenjiemura@gmail.com>
2021-06-21 00:31:12 +0900

    Changed the views into API endpoints

--- tests/onlinestore/products/urls.py
+++ tests/onlinestore/products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+# from .views import ProductListView, ProductDetailView
+from .views import product_list
 
 
 urlpatterns = [
-    path("", ProductListView.as_view(), name="product-list"),
-    path("products/<int:pk>/", ProductDetailView.as_view(),
-         name="product-detail"),
+    # path("", ProductListView.as_view(), name="product-list"),
+    # path("products/<int:pk>/", ProductDetailView.as_view(),
+    #      name="product-detail"),
+    path("products/", product_list, name="product-list")
 ]
--- tests/onlinestore/products/views.py
+++ tests/onlinestore/products/views.py
@@ -1,13 +1,30 @@
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
+# from django.views.generic.detail import DetailView
+# from django.views.generic.list import ListView
+from inspect import getmembers, isclass, isfunction, ismethod
+from django.http import JsonResponse
+
+# class ProductDetailView(DetailView):
+#     model = Product
+#     template_name = "products/product_detail.html"
+
+
+# class ProductListView(ListView):
+#     model = Product
+#     template_name = "products/product_list.html"
 
 from .models import Product, Manufacturer
 
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = "products/product_detail.html"
 
+def product_list(request):
+    products = Product.objects.all()  # We can also get a slice [:30]
 
-class ProductListView(ListView):
-    model = Product
-    template_name = "products/product_list.html"
+    # Let's see what methds does the 'products' object have
+    for (name, member) in getmembers(products, ismethod):
+        if not name.startswith("_"):
+            print(name)
+            # print(member)
+
+    # With the 'values' method, you can specify which columns to retrieve
+    data = {"products": list(products.values("pk", "name"))}
+    response = JsonResponse(data)
+    return response
