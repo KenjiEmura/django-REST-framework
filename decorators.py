e0e5fc2b693e55c3368a026f8d77344cbf776f04
Kenji Emura <kenjiemura@gmail.com>
2021-06-23 00:46:51 +0900

    Practicing python decorators

---
+++ decorators.py
@@ -0,0 +1,21 @@
+def f1(func):
+    def wrapper(*args, **kwargs):
+        print("Started")
+        val = func(*args, **kwargs)
+        print("Ended")
+        return val
+    return wrapper
+
+
+@f1
+def f(a, b=9):
+    print(a, b)
+
+
+@f1
+def add(x, y):
+    print("Inside add function")
+    return x + y
+
+
+print(add(4, 5))
